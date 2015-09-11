5e82efc2633a4984102a97b3b1a46b3b07961c1b
y.zz <yz0437@gmail.com>
2015-09-11 21:33:06 +0800

    no message

---
+++ lua/string.lua
@@ -0,0 +1,25 @@
+
+
+function  trim(str)
+	-- return string.gsub(str,'^%s*(.-)%s*$','%1')
+	-- return (string.gsub(str,'^%s*(.-)%s*$','%1')) ---> 无法删除末尾的空白字符
+	return (string.gsub(str,'^%s*(.-)%s*$','%1'))  ---》舍弃第二个返回值
+end
+
+p = print
+
+
+p(trim(' dfdfad ddd '))
+
+
+function latex2xml(str)
+	return string.gsub(str,"\\(%a+)(%b{})",function (tag,body)
+		body = string.sub(body,2,-2)
+		body = latex2xml(body)
+		return string.format("<%s>%s<%s>",tag,body,tag)
+	end)
+end
+
+p((latex2xml("\\title{the \\bold{big} example}")))
+
+io.read()
